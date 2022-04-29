336957652b8fa4e7a4853fca2307f7097fbf06ba
IT Üzemeltetés <itlab@aut.bme.hu>
2022-04-29 11:25:17 +0200

    Update test cases

--- UltimateAppTest/UnitTest1.cs
+++ UltimateAppTest/UnitTest1.cs
@@ -5,16 +5,37 @@ namespace UltimateApp.Test
     [TestClass]
     public class CalculatorTest
     {
-            [TestMethod]
-            public void TestMethod1()
-            {
-            }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
 
-            [TestMethod]
-            public void AddTest()
-            {
-                int? retVal = Calculator.calculate(2, 3, Operation.Add);
-                Assert.AreEqual(5, retVal);
-            }
+        [TestMethod]
+        public void AddTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Add);
+            Assert.AreEqual(5, retVal);
+        }
+
+        [TestMethod]
+        public void SubstractTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Subtract);
+            Assert.AreEqual(-1, retVal);
+        }
+
+        [TestMethod]
+        public void MultiplyTest()
+        {
+            int? retVal = Calculator.calculate(2, 3, Operation.Multiply);
+            Assert.AreEqual(6, retVal);
+        }
+
+        [TestMethod]
+        public void DivideTest()
+        {
+            int? retVal = Calculator.calculate(3, 3, Operation.Divide);
+            Assert.AreEqual(1, retVal);
+        }
     }
 }
